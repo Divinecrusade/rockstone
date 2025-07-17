2ad76531f8a861faf25aca776284dde955fe63c5
Divinecrusade <gorlovdima@inbox.ru>
2025-07-17 09:29:35 +0300

    style: Remove redument signs '*' in the query's commentary

--- Part 3/select.sql
+++ Part 3/select.sql
@@ -8,7 +8,7 @@ GROUP BY `device`
 ORDER BY 2 DESC
 LIMIT 5;
 
--- Получить **среднее число логинов в день** за последние 7 дней 
+-- Получить среднее число логинов в день за последние 7 дней 
 -- (в расчете от текущей даты)
 SELECT ROUND(COUNT(*) / COUNT(DISTINCT DATE(`login_time`)), 2) AS `avg_logins_per_day`
 FROM players
