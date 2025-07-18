5463bb80d5c00be5b45d7dfc93427071653e0a5e
Divinecrusade <gorlovdima@inbox.ru>
2025-07-18 11:11:07 +0300

    feat: Make tests able to run async

--- Part 2/TopTracker/UnitTests/main.cpp
+++ Part 2/TopTracker/UnitTests/main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <string_view>
 #include <thread>
+#include <array>
+#include <ranges>
+#include <algorithm>
+#include <future>
+#include <execution>
+#include <format>
 
 #include "../TopTracker/TopTracker.h"
 
@@ -9,24 +15,16 @@ namespace test
 {
 	inline namespace stream_output
 	{
-		inline namespace colors
-		{
-			static constexpr std::string_view RED = "\033[31m";
-			static constexpr std::string_view GREEN = "\033[32m";
-			static constexpr std::string_view RESET = "\033[0m";
-
-			static constexpr std::string_view TAG_SUCCESS = "[OK]   ";
-			static constexpr std::string_view TAG_FAILURE = "[FAIL] ";
-		}
-
 		static void print_test_passed(std::string_view msg) noexcept
 		{
-			std::clog << GREEN << TAG_SUCCESS << RESET << msg << std::endl;
+			static constexpr std::string_view SUCCESS_FMT = "\033[32m[OK]   \033[0m{}\n\r";
+			std::clog << std::format(SUCCESS_FMT, msg);
 		}
 
 		static void print_test_failed(std::string_view msg) noexcept
 		{
-			std::cerr << RED << TAG_FAILURE << RESET << msg << std::endl;
+			static constexpr std::string_view FAIL_FMT = "\033[31m[FAIL] \033[0m{}\n\r";
+			std::cerr << std::format(FAIL_FMT, msg);
 		}
 	}
 
@@ -34,58 +32,71 @@ namespace test
 	{
 		namespace is_noexcept
 		{
-			static constexpr bool constructible_player_action() noexcept
+			static void constructible_player_action() noexcept
 			{
-				using PA = PlayerAction;
-				return std::is_nothrow_constructible_v<PA, PA::PlayerId, PA::Type>;
+				if constexpr (std::is_nothrow_constructible_v<PlayerAction, PlayerAction::PlayerId, PlayerAction::Type>)
+					print_test_passed("PlayerAction is noexcept-constructible");
+				else
+					print_test_failed("PlayerAction is NOT noexcept-constructible");
 			}
 
-			static constexpr bool copyable_player_action() noexcept
+			static void copyable_player_action() noexcept
 			{
-				using PA = PlayerAction;
-				return std::is_nothrow_copy_constructible_v<PA> &&
-					std::is_nothrow_move_constructible_v<PA> &&
-					std::is_nothrow_copy_assignable_v<PA> &&
-					std::is_nothrow_move_assignable_v<PA> &&
-					std::is_nothrow_destructible_v<PA>;
+				if constexpr (std::is_nothrow_copy_constructible_v<PlayerAction> &&
+							  std::is_nothrow_move_constructible_v<PlayerAction> &&
+							  std::is_nothrow_copy_assignable_v<PlayerAction> &&
+							  std::is_nothrow_move_assignable_v<PlayerAction> &&
+							  std::is_nothrow_destructible_v<PlayerAction>)
+					print_test_passed("PlayerAction is fully noexcept-copyable and destructible");
+				else
+					print_test_failed("PlayerAction is NOT fully noexcept-copyable or destructible");
 			}
 
-			static constexpr bool public_interface_player_action() noexcept
+			static void public_interface_player_action() noexcept
 			{
-				using PA = PlayerAction;
+				constexpr bool get_id = noexcept(std::declval<const PlayerAction&>().get_player_id());
+				constexpr bool get_type = noexcept(std::declval<const PlayerAction&>().get_type());
+				constexpr bool get_time = noexcept(std::declval<const PlayerAction&>().get_time_stamp());
 
-				constexpr bool get_id = noexcept(std::declval<const PA&>().get_player_id());
-				constexpr bool get_type = noexcept(std::declval<const PA&>().get_type());
-				constexpr bool get_time = noexcept(std::declval<const PA&>().get_time_stamp());
-
-				return get_id && get_type && get_time;
+				if constexpr (get_id && get_type && get_time)
+					print_test_passed("PlayerAction public interface methods are noexcept");
+				else
+					print_test_failed("PlayerAction public interface methods are NOT noexcept");
 			}
 
-			static constexpr bool constructible_toptracker() noexcept
+			static void constructible_toptracker() noexcept
 			{
-				using TT = TopTracker;
-				using Sec = std::chrono::seconds;
-				return std::is_nothrow_constructible_v<TT, Sec, std::size_t>;
+				if constexpr (std::is_nothrow_constructible_v<TopTracker, std::chrono::seconds, std::size_t>)
+					print_test_passed("TopTracker is noexcept-constructible");
+				else
+					print_test_failed("TopTracker is NOT noexcept-constructible");
 			}
 
-			static constexpr bool public_interface_toptracker() noexcept
+			static void public_interface_toptracker() noexcept
 			{
-				using TT = TopTracker;
-				using PA = PlayerAction;
+				constexpr bool on_act = noexcept(std::declval<TopTracker&>().on_action(0, PlayerAction::Type::BUY));
+				constexpr bool del_old = noexcept(std::declval<TopTracker&>().delete_old_actions());
+				constexpr bool view = noexcept(std::declval<const TopTracker&>().get_actions_view());
+				constexpr bool copy = noexcept(std::declval<const TopTracker&>().get_actions_copy());
 
-				constexpr bool on_act = noexcept(std::declval<TT&>().on_action(0, PA::Type::BUY));
-				constexpr bool del_old = noexcept(std::declval<TT&>().delete_old_actions());
-				constexpr bool view = noexcept(std::declval<const TT&>().get_actions_view());
-				constexpr bool copy = noexcept(std::declval<const TT&>().get_actions_copy());
-
-				return on_act && del_old && view && copy;
+				if constexpr (on_act && del_old && view && copy)
+					print_test_passed("TopTracker public interface methods are noexcept");
+				else
+					print_test_failed("TopTracker public interface methods are NOT noexcept");
 			}
+
+			static constexpr std::array TESTS
+			{
+				constructible_player_action, copyable_player_action,
+				public_interface_player_action, constructible_toptracker,
+				public_interface_toptracker
+			};
 		}
 	}
 
 	namespace runtime
 	{
-		static bool basic_insertion()
+		static void basic_insertion()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 5);
 
@@ -94,12 +105,17 @@ namespace test
 
 			const auto& actions = tracker.get_actions_view();
 
-			return actions.size() == 2 &&
-				actions[0].get_player_id() == 1 &&
-				actions[1].get_player_id() == 2;
+			const bool passed = actions.size() == 2 &&
+								actions[0].get_player_id() == 1 &&
+								actions[1].get_player_id() == 2;
+			
+			if (passed)
+				print_test_passed("Basic insertion works");
+			else
+				print_test_failed("Basic insertion failed");
 		}
 
-		static bool capacity_limit()
+		static void capacity_limit()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 3);
 
@@ -110,13 +126,18 @@ namespace test
 
 			const auto& actions = tracker.get_actions_view();
 
-			return actions.size() == 3 &&
-				actions[0].get_player_id() == 2 &&
-				actions[1].get_player_id() == 3 &&
-				actions[2].get_player_id() == 4;
+			const bool passed = actions.size() == 3 &&
+								actions[0].get_player_id() == 2 &&
+								actions[1].get_player_id() == 3 &&
+								actions[2].get_player_id() == 4;
+			
+			if (passed)
+				print_test_passed("Capacity limit and pop_front logic works");
+			else
+				print_test_failed("Capacity limit logic failed");
 		}
 
-		static bool timeout_cleanup()
+		static void timeout_cleanup()
 		{
 			using namespace std::chrono_literals;
 			TopTracker tracker(2s, 10);
@@ -128,10 +149,16 @@ namespace test
 			tracker.delete_old_actions();
 
 			const auto& actions = tracker.get_actions_view();
-			return actions.size() == 1 && actions[0].get_player_id() == 11;
+
+			const bool passed = actions.size() == 1 && actions[0].get_player_id() == 11;
+
+			if (passed)
+				print_test_passed("Timeout cleanup works correctly");
+			else
+				print_test_failed("Timeout cleanup failed");
 		}
 
-		static bool action_ordering()
+		static void action_ordering()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 5);
 
@@ -141,13 +168,18 @@ namespace test
 
 			const auto v = tracker.get_actions_copy();
 
-			return v.size() == 3 &&
-				v[0].get_player_id() == 1 &&
-				v[1].get_player_id() == 2 &&
-				v[2].get_player_id() == 3;
+			const bool passed = v.size() == 3 &&
+								v[0].get_player_id() == 1 &&
+								v[1].get_player_id() == 2 &&
+								v[2].get_player_id() == 3;
+
+			if (passed)
+				print_test_passed("Action ordering is preserved");
+			else
+				print_test_failed("Action ordering is broken");
 		}
 
-		static bool copy_interface()
+		static void copy_interface()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 2);
 			tracker.on_action(7, PlayerAction::Type::SELL);
@@ -155,12 +187,17 @@ namespace test
 
 			const auto copy = tracker.get_actions_copy();
 
-			return copy.size() == 2 &&
-				copy[0].get_player_id() == 7 &&
-				copy[1].get_player_id() == 8;
+			const bool passed = copy.size() == 2 &&
+								copy[0].get_player_id() == 7 &&
+								copy[1].get_player_id() == 8;
+
+			if (passed)
+				print_test_passed("Copy interface returns correct data");
+			else
+				print_test_failed("Copy interface returned incorrect result");
 		}
 
-		static bool monotonic_timestamps()
+		static void monotonic_timestamps()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 10);
 			for (int i = 0; i < 5; ++i)
@@ -170,27 +207,40 @@ namespace test
 			}
 
 			const auto& v = tracker.get_actions_view();
+			bool passed = true;
 			for (size_t i = 1; i < v.size(); ++i)
 			{
 				if (v[i].get_time_stamp() < v[i - 1].get_time_stamp())
-					return false;
+				{
+					passed = false;
+					break;
+				}
 			}
-			return true;
+			if (passed)
+				print_test_passed("Actions is ordered correctly by timestamp");
+			else
+				print_test_failed("Actions is not correctly ordered by timestamp");
 		}
 
-		static bool duplicate_actions_allowed()
+		static void duplicate_actions_allowed()
 		{
 			TopTracker tracker(std::chrono::seconds{5}, 10);
 			tracker.on_action(42, PlayerAction::Type::SELL);
 			tracker.on_action(42, PlayerAction::Type::SELL);
 
 			const auto& v = tracker.get_actions_view();
-			return v.size() == 2 &&
-				v[0].get_player_id() == 42 &&
-				v[1].get_type() == PlayerAction::Type::SELL;
+			const bool passed = v.size() == 2 &&
+								v[0].get_player_id() == 42 &&
+								v[1].get_type() == PlayerAction::Type::SELL;
+
+
+			if (passed)
+				print_test_passed("Duplicated actions are saving");
+			else
+				print_test_failed("Duplicated actions are not saving");
 		}
 
-		static bool delete_old_actions_keeps_fresh()
+		static void delete_old_actions_keeps_fresh()
 		{
 			using namespace std::chrono_literals;
 			TopTracker tracker(1s, 10);
@@ -202,10 +252,15 @@ namespace test
 			tracker.delete_old_actions();
 
 			const auto& v = tracker.get_actions_view();
-			return v.size() == 1 && v[0].get_player_id() == 2;
+			const bool passed = v.size() == 1 && v[0].get_player_id() == 2;
+
+			if (passed)
+				print_test_passed("Deletion and detection of expired actions are correct");
+			else
+				print_test_failed("Deletion and detection of expired actions are not correct");
 		}
 
-		static bool get_copy_equals_view()
+		static void get_copy_equals_view()
 		{
 			TopTracker tracker(std::chrono::seconds{10}, 5);
 			tracker.on_action(1, PlayerAction::Type::BUY);
@@ -214,17 +269,27 @@ namespace test
 			auto copy = tracker.get_actions_copy();
 			const auto& view = tracker.get_actions_view();
 
-			if (copy.size() != view.size()) return false;
-
-			for (size_t i = 0; i < copy.size(); ++i)
+			bool passed = true;
+			if (copy.size() != view.size()) passed = false;
+			else
 			{
-				if (copy[i].get_player_id() != view[i].get_player_id())
-					return false;
+				for (size_t i = 0; i < copy.size(); ++i)
+				{
+					if (copy[i].get_player_id() != view[i].get_player_id())
+					{
+						passed = false;
+						break;
+					}
+				}
 			}
-			return true;
+
+			if (passed)
+				print_test_passed("Copy and view getters return same actions");
+			else
+				print_test_failed("Copy and view getters return different actions");
 		}
 	
-		static bool thread_unsafe_append()
+		static void thread_unsafe_append()
 		{
 			constexpr int thread_count = 8;
 			constexpr int actions_per_thread = 1000;
@@ -259,102 +324,46 @@ namespace test
 			const auto& v = tracker.get_actions_view();
 			bool correct_size = v.size() == static_cast<size_t>(thread_count * actions_per_thread);
 
-			return !failed && correct_size;
+			if (!failed && correct_size)
+				print_test_passed("Multithreaded append worked (threadsafe)");
+			else
+				print_test_failed("Multithreaded append failed (not threadsafe)");
 		}
+
+		static constexpr std::array TESTS
+		{
+			basic_insertion, capacity_limit, timeout_cleanup, 
+			action_ordering, copy_interface, monotonic_timestamps,
+			duplicate_actions_allowed, delete_old_actions_keeps_fresh,
+			get_copy_equals_view, thread_unsafe_append
+		};
 	}
 }
 
 int main()
 {
 	using namespace test::stream_output;
+	const auto run_tests = [](const auto& tests)
+	{
+		const auto future_tests =
+			tests | std::views::transform([](const auto& test) { return std::async(test); });
+		std::for_each(std::execution::par_unseq, future_tests.begin(), future_tests.end(),
+			[](const auto& future_test) { future_test.wait(); });
+	};
 
 	std::clog << ">>> Starts compiletime-noexcept tests <<<\n";
 	{
 		using namespace test::compiletime::is_noexcept;
-
-		if (constructible_player_action())
-			print_test_passed("PlayerAction is noexcept-constructible");
-		else
-			print_test_failed("PlayerAction is NOT noexcept-constructible");
-
-		if (copyable_player_action())
-			print_test_passed("PlayerAction is fully noexcept-copyable and destructible");
-		else
-			print_test_failed("PlayerAction is NOT fully noexcept-copyable or destructible");
-
-		if (public_interface_player_action())
-			print_test_passed("PlayerAction public interface methods are noexcept");
-		else
-			print_test_failed("PlayerAction public interface methods are NOT noexcept");
-
-		if (constructible_toptracker())
-			print_test_passed("TopTracker is noexcept-constructible");
-		else
-			print_test_failed("TopTracker is NOT noexcept-constructible");
-
-		if (public_interface_toptracker())
-			print_test_passed("TopTracker public interface methods are noexcept");
-		else
-			print_test_failed("TopTracker public interface methods are NOT noexcept");
+		run_tests(TESTS);
 	}
 	std::clog << ">>> Ends compiletime-noexcept tests <<<\n";
 
 	std::clog << ">>> Starts runtime tests <<<\n";
 	{
 		using namespace test::runtime;
-
-		if (basic_insertion())
-			print_test_passed("Basic insertion works");
-		else
-			print_test_failed("Basic insertion failed");
-
-		if (capacity_limit())
-			print_test_passed("Capacity limit and pop_front logic works");
-		else
-			print_test_failed("Capacity limit logic failed");
-		
-		if (timeout_cleanup())
-			print_test_passed("Timeout cleanup works correctly");
-		else
-			print_test_failed("Timeout cleanup failed");
-
-		if (action_ordering())
-			print_test_passed("Action ordering is preserved");
-		else
-			print_test_failed("Action ordering is broken");
-
-		if (copy_interface())
-			print_test_passed("Copy interface returns correct data");
-		else
-			print_test_failed("Copy interface returned incorrect result");
-
-		if (monotonic_timestamps())
-			print_test_passed("Actions is ordered correctly by timestamp");
-		else
-			print_test_failed("Actions is not correctly ordered by timestamp");
-
-		if (duplicate_actions_allowed())
-			print_test_passed("Duplicated actions are saving");
-		else
-			print_test_failed("Duplicated actions are not saving");
-
-		if (delete_old_actions_keeps_fresh())
-			print_test_passed("Deletion and detection of expired actions are correct");
-		else
-			print_test_failed("Deletion and detection of expired actions are not correct");
-
-		if (get_copy_equals_view())
-			print_test_passed("Copy and view getters return same actions");
-		else
-			print_test_failed("Copy and view getters return different actions");
-
-		if (thread_unsafe_append())
-			print_test_passed("Multithreaded append worked (threadsafe)");
-		else
-			print_test_failed("Multithreaded append failed (not threadsafe)");
+		run_tests(TESTS);
 	}
 	std::clog << ">>> Ends runtime tests <<<\n";
-
 
 	return 0;
 }
